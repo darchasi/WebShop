b50025f104ee3c43445d287abfafa06dd5e15e19
darchasi <pv20qck@eduvaud.ch>
2024-05-03 08:36:37 +0200

    commit 4 model avec mysql

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,72 +1,40 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("../config/database");
+// userModel.js
+const db = require("../db/mock-users");
 
-const UserModel = sequelize.define("User", {
-  id: {
-    type: Datatypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  firstName: {
-    type: Datatypes.STRING,
-    allowNull: false,
-    validate: {
-      is: {
-        args: /^[A-Za-z0-9\s]/,
-        msg: "Seules les lettres, les chiffres et les espaces sont autorisées.",
-      },
-      notEmpty: {
-        msg: "Le prénom de l'utilisateur ne peut pas être vide.",
-      },
-      notNull: {
-        msg: "Le prénom de l'utilisateur est une propriété obligatoire ",
-      },
-      len: {
-        args: [0, 35],
-        msg: "Le nom de l'utilisateur doit contenir au maximum 35 caractères.",
-      },
-    },
-  },
-  lastName: {
-    type: Datatypes.STRING,
-    allowNull: false,
-    validate: {
-      is: {
-        args: /^[A-Za-z0-9\s]/,
-        msg: "Seules les lettres, les chiffres et les espaces sont autorisées.",
-      },
-      notEmpty: {
-        msg: "Le nom de l'utilisateur ne peut pas être vide.",
-      },
-      notNull: {
-        msg: "Le nom de l'utilisateur est une propriété obligatoire.",
-      },
-      len: {
-        args: [0, 35],
-        msg: "Le nom de l'utilisateur doit contenir au maximum 35 caractères.",
-      },
-    },
-  },
-  nickName: {
-    type: Datatypes.STRING,
-    allowNull: false,
-    validate: {
-      is: {
-        args: /^[A-Za-z0-9\s]/,
-        msg: "Seules les lettres, les chiffres et les espaces sont autorisées.",
-      },
-      notEmpty: {
-        msg: "Le pseudo de l'utilisateur ne peut pas être vide.",
-      },
-      notNull: {
-        msg: "Le pseudo de l'utilisateur est une propriété obligatoire.",
-      },
-      len: {
-        args: [0, 35],
-        msg: "Le pseudo de l'utilisateur doit contenir au maximum 35 caractères.",
-      },
-    },
-  },
-});
+const validateUser = (user) => {
+  const errors = [];
+  const regex = /^[A-Za-z0-9\s]+$/;
 
-module.exports = UserModel;
+  // Validación de firstName
+  if (!user.firstName || user.firstName.trim().length === 0)
+    errors.push("Le prénom de l'utilisateur ne peut pas être vide.");
+  if (user.firstName.length > 35)
+    errors.push(
+      "Le nom de l'utilisateur doit contenir au maximum 35 caractères."
+    );
+  if (!regex.test(user.firstName))
+    errors.push(
+      "Seules les lettres, les chiffres et les espaces sont autorisées."
+    );
+
+  return errors;
+};
+
+const addUser = async (user) => {
+  const errors = validateUser(user);
+  if (errors.length > 0) {
+    return { errors };
+  }
+
+  const sql = `INSERT INTO User (firstName, lastName, nickName) VALUES (?, ?, ?)`;
+  const result = await db.query(sql, [
+    user.firstName,
+    user.lastName,
+    user.nickName,
+  ]);
+  return result;
+};
+
+module.exports = {
+  addUser,
+};
